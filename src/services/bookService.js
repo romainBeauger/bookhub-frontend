315dbfdc07dc0b21cd509adc1001bb04088a8729
YoussefGhollamallah <youssef.gh@live.fr>
2026-04-17 01:26:56 +0200

    Ajout: création du service détails livre

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -34,3 +34,37 @@ export const getBooks = async ({ page = 1, limit = 10 } = {}) => {
         return await response.json()
     }
 }
+
+export const getBookById = async (bookId) => {
+    const token = sessionStorage.getItem("token");
+    const response = await fetch(`/api/books/${bookId}`, {
+        method: "GET",
+        headers: token
+            ? {
+                Authorization: `Bearer ${token}`,
+            }
+            : undefined,
+    })
+
+    if (!response.ok) {
+        let errorData = null
+
+        try {
+            errorData = await response.json()
+        } catch {
+            errorData = null
+        }
+
+        if (response.status === 401) {
+            throw new Error("Session expirée ou accès non autorisé.")
+        }
+
+        if (response.status === 404) {
+            throw new Error("Livre introuvable.")
+        }
+
+        throw new Error(errorData?.message || "Une erreur est survenue")
+    }
+
+    return await response.json()
+}
