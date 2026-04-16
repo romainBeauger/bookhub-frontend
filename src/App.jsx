21aedf730fe05ebe9cc1db92bd4962b3e55f08c0
romainBeauger <romain.beauger@gmail.com>
2026-04-16 15:18:32 +0200

    feat: ajout tests unitaires LoginForm et RegisterForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import './styles/main.css'
 import AuthPage from "./pages/AuthPage.jsx";
-import BooksPage from "./pages/BooksPage.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import BooksPage from "./pages/BooksPage.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx"
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx"
 
@@ -12,6 +12,12 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
           <Routes>
+
+              {/* Exemple de route protégée qui vérifie le token dans le session storage */}
+              {/*<ProtectedRoute>*/}
+              {/*    <RouteAProteger />*/}
+              {/*</ProtectedRoute>*/}
+
               <Route path="/login" element={<AuthPage defaultTab="login" />} />
               <Route path="/forgot-password" element={<ForgotPasswordPage />} />
               <Route path="/register" element={<AuthPage defaultTab="register" />} />
