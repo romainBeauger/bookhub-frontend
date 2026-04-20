6815e18a37ce467491a731dee7eff386262812f1
YoussefGhollamallah <youssef.gh@live.fr>
2026-04-20 15:26:36 +0200

    up

--- src/components/BooksPage/BooksSidebar.jsx
+++ src/components/BooksPage/BooksSidebar.jsx
@@ -154,7 +154,16 @@ export default function BooksSidebar({
                                 MES AVIS
                             </NavLink>
                         </li>
-                        <li className="px-3 py-1 text-slate-400">(PROFIL)</li>
+                        <li>
+                            <NavLink
+                                to="/profile"
+                                className={({ isActive }) =>
+                                    `block rounded-xl px-3 py-2 font-medium ${isActive ? "bg-slate-200 text-blue-500" : "text-slate-950 hover:bg-slate-100"}`
+                                }
+                            >
+                                MON PROFIL
+                            </NavLink>
+                        </li>
                     </ul>
                 </nav>
 
